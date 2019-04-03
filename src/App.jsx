981a47e71f46d9f9c5116662803165d87970885f
RyanDBarnett <conner2468@gmail.com>
2019-04-02 18:46:18 -0600

    Import datasets and combine into one

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Places from './components/Places.jsx'
-import SearchForm from './components/SearchForm.jsx'
-import Footer from './components/Footer.jsx'
+import Places from './components/Places.jsx';
+import SearchForm from './components/SearchForm.jsx';
+import Footer from './components/Footer.jsx';
+import {climbingPlaces, routes} from './data/climbing-data.js';
 
+let result = climbingPlaces.map( currentPlace => {
+  let newPlace = {};
+  newPlace.place = currentPlace.place;
+  newPlace.closestTown = currentPlace.closestTown;
+  newPlace.climbingId = currentPlace.climbingId;
+  newPlace.routes = routes.filter( route => {
+    return route.climbingPlaceId === newPlace.climbingId
+  });
+  return newPlace;
+});
 
 class App extends Component {
 
@@ -11,7 +22,7 @@ class App extends Component {
     super() 
 
     this.state = {
-      results: []
+      results: result
     }
   }
   
