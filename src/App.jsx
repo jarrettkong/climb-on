26ede4bbbee2494a1da33cc1e99317b62c344135
Lauren Boyer <lboyer4@outlook.com>
2019-04-09 12:50:54 -0600

    renamed variables for readablity
    Co-Authored-By: Ryan Barnett <conner2468@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,16 @@ class App extends Component {
   componentDidMount = () => {
 
     const routePromise = fetch('https://fe-apps.herokuapp.com/api/v1/whateverly/1901/lboyer4/routes')
-    .then(res => res.json())
+      .then(res => res.json());
     
     const placesPromise = fetch('https://fe-apps.herokuapp.com/api/v1/whateverly/1901/lboyer4/climbingPlaces')
-    .then(res => res.json())
+      .then(res => res.json());
     
     Promise.all([routePromise, placesPromise])
-    .then(data => { 
-      const combinedData = this.mergeData(data[0].routes, data[1].climbingPlaces)
-      this.setState({combinedData: combinedData})
-    }).catch(error => console.log(error))
+      .then(data => { 
+        const combinedData = this.mergeData(data[0].routes, data[1].climbingPlaces)
+        this.setState({combinedData: combinedData})
+      }).catch(error => console.log(error))
   }
 
   mergeData = (routes, places) => {
@@ -44,18 +44,18 @@ class App extends Component {
   }
 
   sortByDifficulty = (routes, bool) => {
-    routes.sort((a, b) => {
-      const first = parseInt(a.difficultyLevel.slice(2)
+    routes.sort((routeA, routeB) => {
+      const diffA = parseInt(routeA.difficultyLevel.slice(2)
           .replace('a','1')
           .replace('b','2')
           .replace('c','3')
           .replace('d','4'));
-      const second = parseInt(b.difficultyLevel.slice(2)
+      const diffB = parseInt(routeB.difficultyLevel.slice(2)
           .replace('a','1')
           .replace('b','2')
           .replace('c','3')
           .replace('d','4'));
-      return bool ? first - second : second - first;
+      return bool ? diffA - diffB : diffB - diffA;
     });
   }
 
@@ -65,9 +65,9 @@ class App extends Component {
 
   searchData = () => {
     const query = this.state.searchQuery.toLowerCase();
-    const results = this.state.combinedData.filter(r => {
-      return r.place.toLowerCase().includes(query) ||
-             r.closestTown.toLowerCase().includes(query)
+    const results = this.state.combinedData.filter(currentPlace => {
+      return currentPlace.place.toLowerCase().includes(query) ||
+             currentPlace.closestTown.toLowerCase().includes(query)
     })
     return results;
   }
@@ -112,7 +112,10 @@ class App extends Component {
   }
   
   render() {
-    const searchResults = this.state.combinedData ? this.searchData() : [];
+    let searchResults = this.state.combinedData || [];
+    if(this.state.searchQuery !== '') {
+      searchResults = this.searchData();
+    }
     const results = this.filterResults(searchResults);
 
     return (
