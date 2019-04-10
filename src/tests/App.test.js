2d1b1a512c3c40cf056ef81caa93a500ce607a38
RyanDBarnett <conner2468@gmail.com>
2019-04-10 15:53:52 -0600

    Create test for isRouteDifficultyAllowed method
    Co-authored-by: Lauren Boyer <lboyer4@outlook.com>
    Co-authored-by: Michael King <kingstockton@gmail.com>

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,24 @@ import React from 'react';
 import App from '../App.jsx';
 import { shallow } from "enzyme";
 
+const mockRoute = { 
+  "climbingPlaceId": 1,
+  "area": "Brown Cloud Crags",
+  "routeName": "Louise",
+  "difficultyLevel": "5.8",
+  "type": ["sport"] 
+}
+
+const mockRoute2 = { 
+  "climbingPlaceId": 1,
+  "area": "Brown Cloud Crags",
+  "routeName": "Louise",
+  "difficultyLevel": "5.9",
+  "type": ["sport"] 
+}
+
+const mockAllowedDifficulties = ["5.8"]
+
 describe("App", () => {
 
   let wrapper;
@@ -14,6 +32,16 @@ describe("App", () => {
 	
   it("should match the snapshot with all data passed in", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe("isRouteDifficultyAllowed Method", () => {
+    it("should return true if difficulty is allowed", () => {
+      expect(wrapper.instance().isRouteDifficultyAllowed(mockAllowedDifficulties, mockRoute)).toEqual(true);
+    });
+
+    it("should return false if difficulty is allowed", () => {
+      expect(wrapper.instance().isRouteDifficultyAllowed(mockAllowedDifficulties, mockRoute2)).toEqual(false);
+    });
   });
 
   it("should change filters state when updateFilters is invoked", () => {
